908a342c8201ff2bf77152049aa2b8bf5d2b5e55
Aleksandra Jankowska <janka303@newschool.edu>
2019-04-16 21:50:34 -0400

    second version

--- fft.js
+++ fft.js
@@ -1,45 +1,89 @@
-var song;
-var fft;
-var button;
+let yoff = 0.0; // 2nd dimension of perlin noise
 
-var w;
-
-function toggleSong(){
-  if(song.isPlaying()){
-    song.pause();
-  }else{
-    song.play();
-  }
+function setup() {
+  createCanvas(windowWidth, windowHeight);
 }
 
-function preload(){
-  song = loadSound('sumienie.mp3');
-}
-
-function setup(){
-  createCanvas(windowWidth, 500);
-  colorMode(HSB);
-  angleMode(DEGREES);
-  // button = createButton('toggle');
-  // button.mousePressed(toggleSong);
-  song.play();
-  fft = new p5.FFT(0.9,256);
-  w = width/64;
-  
-}
-
-function draw(){
-  background(255);
-  var spectrum = fft.analyze();
-  for(var i=0; i < spectrum.length; i++){
-    var amp = spectrum[i]/2;
-    var y = map(amp, 0, 256, height, 0);
-    fill(i, 255, 255);
-    rect(i*w, y, w, height-y, 6, 6, 0, 0);
-    rect(i*w, 0, w, height-y, 0, 0, 6, 6);
-  }
-
-  // console.log(spectrum,length);
+function draw() {
+  color(HSB);
+  background('#fc861d');
   noStroke();
-  noFill();
+
+  // var col = lerpColor('#2cdaff', '#fc861d')
+
+  fill(255);
+  // We are going to draw a polygon out of the wave points
+  beginShape();
+
+  // let xoff = 0; // Option #1: 2D Noise
+  let xoff = yoff; // Option #2: 1D Noise
+
+  // Iterate over horizontal pixels
+  for (let x = 0; x <= width; x += 10) {
+    // Calculate a y value according to noise, map to
+
+    // Option #1: 2D Noise
+    let y = map(noise(xoff, yoff), 0, 1, 200, 300);
+
+    // Option #2: 1D Noise
+    // let y = map(noise(xoff), 0, 1, 200,300);
+
+    // Set the vertex
+    vertex(x, y+50);
+    // Increment x dimension for noise
+    xoff += 0.1;
+  }
+  // increment y dimension for noise
+  yoff += 0.01;
+  vertex(width, height);
+  vertex(0, height);
+  endShape(CLOSE);
 }
+
+//--------------------------------------------------------------
+
+// var song;
+// var fft;
+// var button;
+
+// var w;
+
+// function toggleSong(){
+//   if(song.isPlaying()){
+//     song.pause();
+//   }else{
+//     song.play();
+//   }
+// }
+
+// function preload(){
+//   song = loadSound('sumienie.mp3');
+// }
+
+// function setup(){
+//   createCanvas(windowWidth, 500);
+//   colorMode(HSB);
+//   angleMode(DEGREES);
+//   // button = createButton('toggle');
+//   // button.mousePressed(toggleSong);
+//   song.play();
+//   fft = new p5.FFT(0.9,256);
+//   w = width/64;
+  
+// }
+
+// function draw(){
+//   background(255);
+//   var spectrum = fft.analyze();
+//   for(var i=0; i < spectrum.length; i++){
+//     var amp = spectrum[i]/2;
+//     var y = map(amp, 0, 256, height, 0);
+//     fill(i, 255, 255);
+//     rect(i*w, y, w, height-y, 6, 6, 0, 0);
+//     rect(i*w, 0, w, height-y, 0, 0, 6, 6);
+//   }
+
+//   // console.log(spectrum,length);
+//   noStroke();
+//   noFill();
+// }
